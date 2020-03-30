7b9d3860f6de34c94a21ecf47bc5f3221ad77814
Fred C <canofred@gmail.com>
2020-03-30 18:30:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,8 @@
 def join_nested_strings(src)
   new_string = '';
   row_index = 0
+  new_array = []
+  
   while row_index < src.count do 
     element_index = 0 
     while element_index < src[row_index].count do 
